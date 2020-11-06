bff8789b188e74c58698bb468c11d064503c02c6
Peter Rabbitson <ribasushi@protocol.ai>
2020-11-06 13:58:25 +0100

    relpaths for everything

--- fil-cloner.bash
+++ fil-cloner.bash
@@ -33,11 +33,11 @@ for arg in "$@"; do \
     exit 1
   fi
 
-  if /usr/bin/lsblk -n -o mountpoint "${drvMatches[0]}" | grep -v '^$' ; then
+  if lsblk -n -o mountpoint "${drvMatches[0]}" | grep -v '^$' ; then
     echo "Drive with serial number '$arg' seems to be mounted as listed above"
     exit 1
   fi
-  if /usr/bin/lsblk -n -o name,type "${drvMatches[0]}" | grep -vE '(disk|part)$' ; then
+  if lsblk -n -o name,type "${drvMatches[0]}" | grep -vE '(disk|part)$' ; then
     echo "Drive with serial number '$arg' seems to be a building block for another device"
     exit 1
   fi
@@ -48,14 +48,14 @@ done
 srcDrive="${dstDrives[0]}"
 dstDrives=("${dstDrives[@]:1}")
 
-scratchfn="$( /usr/bin/mktemp -t tmp.XXXXXXXXXX )"
+scratchfn="$( mktemp -t tmp.XXXXXXXXXX )"
 ! [[ -r "$scratchfn" ]] && echo "Creation of the templog failed for some reason" && exit 1
 
 onEnd() {
   ex=$?
   set +e
 
-  curl -s "https://fil-discover-drive-clone.s3-ap-east-1.amazonaws.com/$(date +'%s.%N')_${ex}_$( <<<"${srcDrive} ${dstDrives[@]}" /usr/bin/sed 's/ /_/g' | /usr/bin/sed 's/\/dev\/disk\/by-id\///g' | /usr/bin/sed 's/\//-/g' )" \
+  curl -s "https://fil-discover-drive-clone.s3-ap-east-1.amazonaws.com/$(date +'%s.%N')_${ex}_$( <<<"${srcDrive} ${dstDrives[@]}" sed 's/ /_/g' | sed 's/\/dev\/disk\/by-id\///g' | sed 's/\//-/g' )" \
     -H "x-amz-acl: bucket-owner-full-control" -T "$scratchfn"
 
   ! [[ "$ex" = "0" ]] && echo -e "\nSomething went wrong... please DO NOT SHIP the drives as-is, retry instead!!!\n\nIf the problem persists contact @ribasushi\n\n"
@@ -66,12 +66,12 @@ onEnd() {
 trap onEnd EXIT
 
 exec 3>&2
-exec 2> >( /usr/bin/tee -a "$scratchfn" >&3)
-exec 1> >( /usr/bin/tee -a "$scratchfn" >&3)
+exec 2> >( tee -a "$scratchfn" >&3)
+exec 1> >( tee -a "$scratchfn" >&3)
 
 nl=$'\n'
 IFS="$nl"
-now=$( /usr/bin/date -u +'%H:%M:%S' )
+now=$( date -u +'%H:%M:%S' )
 read -p "${nl}It is $now UTC and you are about to IRREVERSIBLY write the contents of${nl}${nl}${srcDrive}${nl}  to${nl}${dstDrives[*]}${nl}${nl}If this is what you intended - enter the current minute MM: " -r
 if [[ "$REPLY" == "" ]] || [[ "$REPLY" != "$( cut -d ':' -f 2 <<<$now )" ]]; then
   echo "Not the expected answer, aborting"
@@ -81,9 +81,9 @@ fi
 echo -e "\nStarting replication...\nNOTE wait for the prompt to return, there might be a 30 second delay after the last printout\n\n"
 
 IFS=" "
-finCmd="/usr/bin/dd bs=32M status=progress if=$srcDrive"
+finCmd="dd bs=32M status=progress if=$srcDrive"
 for d in "${dstDrives[@]}"; do
-  finCmd="$finCmd | /usr/bin/tee >( /usr/bin/dd bs=32M status=none of=$d)"
+  finCmd="$finCmd | tee >( dd bs=32M status=none of=$d)"
 done
 finCmd="$finCmd > /dev/null"
 eval $finCmd
